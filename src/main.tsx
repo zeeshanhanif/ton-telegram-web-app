4e49e3a38d68466b3c508dea78b304a77061337b
zeeshanhanif <zeeshanhanif@gmail.com>
2024-12-03 23:36:21 +0500

    Added main.tsx file

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import { TonConnectUIProvider } from '@tonconnect/ui-react';
+
+// this manifest is used temporarily for development purposes
+const manifestUrl = 'https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json';
+
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <TonConnectUIProvider manifestUrl={manifestUrl}>
+      <App />
+    </TonConnectUIProvider>
+  </StrictMode>,
+)

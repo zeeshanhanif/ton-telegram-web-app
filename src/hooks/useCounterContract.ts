c5c67c9616b226ceba5f777931396759c2e3ff36
zeeshanhanif <zeeshanhanif@gmail.com>
2024-12-06 23:50:55 +0500

    Added useCounterContract hook

---
+++ src/hooks/useCounterContract.ts
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react';
+import { CheckCounter as Counter } from '../contracts/CheckCounter';
+import { useTonClient } from './useTonClient';
+import { useAsyncInitialize } from './useAsyncInitialize';
+import { useTonConnect } from './useTonConnect';
+import { Address, OpenedContract, toNano } from '@ton/core';
+
+export function useCounterContract() {
+  const client = useTonClient();
+  const [val, setVal] = useState<null | string>();
+  const { sender } = useTonConnect();
+
+  const sleep = (time: number) => new Promise((resolve) => setTimeout(resolve, time));
+
+  const counterContract = useAsyncInitialize(async () => {
+    if (!client) return;
+    const contract = new Counter(
+      Address.parse('EQBwsKUsMTjwGc29K19JUcP8hlElJq4n1J002RRqkOWamrpi') // replace with your address from tutorial 2 step 8
+    );
+    return client.open(contract) as OpenedContract<Counter>;
+  }, [client]);
+
+  useEffect(() => {
+    async function getValue() {
+      if (!counterContract) return;
+      setVal(null);
+      const val = await counterContract.getCounter();
+      setVal(val.toString());
+      await sleep(5000); // sleep 5 seconds and poll value again
+      getValue();
+    }
+    getValue();
+  }, [counterContract]);
+
+  return {
+    value: val,
+    address: counterContract?.address.toString(),
+    sendIncrement: () => {
+      return counterContract?.sendIncrease(sender, {
+        increaseBy: 1,
+        value: toNano('0.002'),
+      });
+    },
+  };
+}
